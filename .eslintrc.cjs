d4b28156bb2044818f194cd066997fd8db978485
Max Levytskyi <immaxlev@gmail.com>
2023-03-04 18:28:10 +0200

    first commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  env: {
+    es2020: true,
+  },
+  extends: ["prettier"],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  parser: "@typescript-eslint/parser",
+  rules: {},
+};
---
+++ rollup.config.cjs
@@ -0,0 +1,38 @@
+const nodeResolve = require("@rollup/plugin-node-resolve");
+const typescript = require("rollup-plugin-typescript2");
+const dts = require("rollup-plugin-dts");
+const filesize = require("rollup-plugin-filesize");
+const json = require("@rollup/plugin-json");
+
+const base = {
+  external: ["tmp"],
+  plugins: [
+    typescript(),
+    json(),
+    nodeResolve({ extensions: [".js", ".ts"] }),
+    filesize(),
+  ],
+};
+
+module.exports = [
+  {
+    input: "./dts/index.d.ts",
+    output: [{ file: "lib/index.d.ts", format: "es" }],
+    plugins: [dts.default()],
+  },
+  {
+    ...base,
+    input: "./src/index.ts",
+    external: [...base.external],
+    output: [
+      {
+        file: "lib/index.js",
+        format: "cjs",
+      },
+      {
+        file: "lib/index.esm.js",
+        format: "es",
+      },
+    ],
+  },
+];
