1be6abfbae5d19ea02e401a0302940ac09689286
Max Levytskyi <immaxlev@gmail.com>
2024-07-21 00:57:44 +0300

    change baseUrl

--- web/docusaurus.config.ts
+++ web/docusaurus.config.ts
@@ -12,7 +12,7 @@ const config: Config = {
   url: "https://gmaxlev.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/pict-node/",
+  baseUrl: "/pict-node",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
